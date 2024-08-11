0a0804632bcf28d5c2346e933db2aacb4be17cad
marcianomatias <marcianomatias1@hotmail.com>
2024-08-10 22:18:20 -0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+import profilepic from "../assets/profpic.png";
+import { TypeAnimation } from "react-type-animation";
+import ShinyEffect from "./ShinyEffect";
+import {
+  AiOutlineGithub,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineWhatsApp
+} from "react-icons/ai";
+import {
+  DiCss3,
+  DiHtml5,
+  DiJavascript1,
+  DiNodejsSmall,
+  DiReact,
+  DiUbuntu,
+  DiPython,
+  DiMysql,
+} from "react-icons/di";
+import { motion } from "framer-motion";
+import curriculo from '../assets/Curriculo.pdf'; // Importando o currículo
+
+
+const Hero = () => {
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = curriculo; // Utilizando o caminho importado do PDF
+    link.setAttribute('download', 'curriculo.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className="mt-24 max-w-[1200px] mx-auto relative">
+        <div className="grid md:grid-cols-2 place-items-center gap-8">
+            <motion.div
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            >
+                <TypeAnimation
+                    sequence={[
+                        "Fullstack Dev",
+                        1000,
+                        "Web developer",
+                        1000,
+                        "Data Science",
+                        1000
+                    ]}
+                    speed={50}
+                    repeat={Infinity}
+                    className="font-bold text-gray-400 text-xl md:text-5xl italic mb-2"
+                />
+
+                <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 0.5 }}
+                className="text-gray-200 md:text-7xl text-5xl tracking-tight mb-4"
+                >
+                    Hey 🖖🏾, Eu sou o <br/>
+                    <span className="text-purple-500">Marciano Matias</span>
+                </motion.p>
+
+                <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 1 }}
+                className="text-gray-300 max-w-[300px] md:max-w-[500px] md:text-2xl text-lg mb-6"
+                >
+                    Estou determinado a aprender rapidamente e contribuir com soluções inovadoras, mesmo sem experiência prática.
+                </motion.p>
+
+                <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1, delay: 1.5 }}
+                className="flex flex-row items-center gap-6 my-4 md:mb-0"
+                >
+                    
+                    <motion.button 
+                        whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)" }}
+                        onClick={handleDownload}
+                        className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
+                                     border-purple-400 rounded-xl"     
+                    >
+                        Download CV
+                    </motion.button>
+
+                    <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20">
+                        <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/marcianomatias" target="_blank">
+                            <AiOutlineGithub/>
+                        </motion.a>
+
+                        <motion.a whileHover={{ scale: 1.2 }} href="https://www.linkedin.com/in/marcianomatias/" target="_blank">
+                            <AiOutlineLinkedin/>
+                        </motion.a>
+
+                        <motion.a whileHover={{ scale: 1.2 }} href="https://wa.me/5522997165097?text=Ol%C3%A1%20seja%20bem%20vindo!" target="_blank">
+                            <AiOutlineWhatsApp/>
+                        </motion.a>
+                    </div>
+                </motion.div>
+            </motion.div>
+
+
+            <motion.img 
+                src={profilepic}
+                className="w-[300px] md:w-[450px]"
+                initial={{ opacity: 0, scale: 0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: true }}
+                transition={{ duration: 1 }}
+            />
+        </div>
+
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 2 }}
+            className="flex flex-row text-7xl px-12 md:px-0 w-full justify-center items-center py-24"
+        >
+            <p className="text-gray-200 mr-6">My Tech Stack</p>
+            <DiHtml5 className="text-orange-600 mx-2" />
+            <DiCss3 className="text-blue-600 mx-2" />
+            <DiJavascript1 className="text-yellow-500 mx-2" />
+            <DiReact className="text-blue-500 mx-2" />
+            <DiNodejsSmall className="text-green-500 mx-2" />
+            <DiUbuntu className="text-orange-500 mx-2" />
+            <DiPython className="text-yellow-400 mx-2" />
+            <DiMysql className="text-blue-400 mx-2" />
+           
+
+        </motion.div>
+
+        <div className="absolute inset-0 hidden md:block">
+            <ShinyEffect left={0} top={0} size={1400} />
+        </div>
+        
+    </div>
+  )
+}
+
+export default Hero;
